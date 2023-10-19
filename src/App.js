90c00ea4ba3a5608d7a38ec89333bcdd2d4a04fe
Berly Gustavo <gustav.alave@gmail.com>
2023-10-19 09:54:39 -0500

    v3. Instalo y conf App, AppForm, bootwatch, mat.icons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Appform from './component/Appform';
+import C01componente from './component/C01componente';
+import Variable from './component/C04variable';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Gustavo Alave <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div 
+    style={{background:"yellow", 
+    width:"350px", 
+    padding:"10px"}}>
+      <Appform/>
+      <i class="large material-icons">insert_chart</i>
+    
     </div>
   );
 }
