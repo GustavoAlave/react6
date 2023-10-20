a8451d7b64ef34bb3b8848d5c96586daa6804b1a
BerlyGus <berly.gustav@gmail.com>
2023-10-20 09:09:12 -0500

    v6 Se Instalo fb -auth-storage-firestore

--- src/App.js
+++ src/App.js
@@ -11,6 +11,10 @@ function App() {
     padding:"10px"}}>
       <Appform/>
       <i class="large material-icons">insert_chart</i>
+
+      <p>1.Juan Manuel 23 Masculino  ----x -- A</p>
+      <p>2.Rosa Maria  25 Femenino   ----x -- A</p>
+      <p>3.Luis Miguel 23 Masculino  ----x -- A</p>
     
     </div>
   );
